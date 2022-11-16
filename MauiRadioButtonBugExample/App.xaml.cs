2faadb4f6fdcc6defd337ca36042ad4a9b0d732d
andycambo <andywcampbell@hotmail.com>
2022-11-16 15:31:05 +0000

    Add project files.

---
+++ MauiRadioButtonBugExample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiRadioButtonBugExample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiRadioButtonBugExample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiRadioButtonBugExample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
